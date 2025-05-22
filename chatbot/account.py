a69c27dc3f66df81da6e45e05e9d2c7d58c67449
Hsi-Teng Liu <frogcoder@gmail.com>
2025-05-22 12:11:46 -0400

    initialize test database and authenticate from database

--- chatbot/account.py
+++ chatbot/account.py
@@ -1,13 +1,5 @@
 from decimal import Decimal
-
-
-class Account:
-    """Represent an account of a user""" 
-    account_number: str
-    """The unique account number."""
-    
-    account_name: str
-    """Name of the account."""
+from models import Account
 
 
 def list_accounts(user_id: str) -> list[Account]:
